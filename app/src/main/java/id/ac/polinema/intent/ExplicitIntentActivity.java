fe50b25f0eab20e6fa1d85c2f9ef916ae1bc33a4
larasatizka <larasatizka@gmail.com>
2020-02-18 20:04:46 +0700

    Perubahan telah dilakukan

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,35 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    EditText etHasil;
+    Button bHasil;
+    TextView tvHasil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+        etHasil=findViewById(R.id.input_name);
+        bHasil=findViewById(R.id.submit);
+        tvHasil=findViewById(R.id.text_output);
+
+        bHasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String hasil=etHasil.getText().toString();
+                tvHasil.setText("Hello " +hasil+", Congratulations!");
+            }
+        });
     }
 
 }
--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -1,6 +1,8 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void handleExplicitIntent(View view) {
+        Intent intent = new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleExit(View view) {
+        finish();
+    }
 }
